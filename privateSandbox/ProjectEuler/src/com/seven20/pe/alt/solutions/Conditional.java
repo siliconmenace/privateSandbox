fc15586fbc4752513338769acc269af6db2ab9c3
Jerry Davison <tesla@tesla.(none)>
2014-01-05 03:46:51 -0600

    Deleted dup repo

--- privateSandbox/ProjectEuler/src/com/seven20/pe/alt/solutions/Conditional.java
+++
@@ -1,9 +0,0 @@
-package com.seven20.pe.alt.solutions;
-
-public interface Conditional<T>
-{
-	public boolean validate(T arg);
-
-	public boolean f( int i, int j, int k );
-	
-}
--- privateSandbox/ProjectEuler/src/com/seven20/pe/problems/pokerhands/ScoreProcessor.java
+++
@@ -1,7 +0,0 @@
-package com.seven20.pe.problems.pokerhands;
-
-public abstract class ScoreProcessor {
-	
-	public abstract HandValue handScore(Hand h);
-
-}
